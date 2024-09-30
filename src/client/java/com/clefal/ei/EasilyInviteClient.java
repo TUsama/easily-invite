2fe4c60ab721fb0d4ed7c0173ce0087285a4b859
TUsama <1716976420@qq.com>
2024-09-30 18:15:16 +0800

    init

---
+++ src/client/java/com/clefal/ei/EasilyInviteClient.java
@@ -0,0 +1,10 @@
+package com.clefal.ei;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasilyInviteClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
